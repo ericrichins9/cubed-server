3ad8a23994159fabf159dd8bab0472a1ee0eee09
Eric Richins <ericrichins9@gmail.com>
2022-04-20 20:48:10 -0600

    create and join game functional (not pretty)

--- index.js
+++ index.js
@@ -10,68 +10,42 @@ app.use((_req, res) => res.sendFile(INDEX, { root: __dirname }))
 
 const server = app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}...`));
 
-// socket server
 const socket = require('socket.io');
 const io = socket(server);
 
-let players = []
-let turnOrder = 1
-let currentPlayer = {}
+let rooms = {}
 
-io.on('connection', (socket) => {
-    socket.on('newPlayer', data => {
-        console.log("CONNECTED", socket.id)
-            data.id = socket.id
-            if(players.length < 3){
-                players.push(data)
-            }
-            if (players.length === 1){
-                players[0].isMyTurn = true
-                currentPlayer = players[0]
-            }
-            else if (players.length === 2) {
-                players[1].name = "Michelle"
-                players[1].turnOrder = 2
-                players[1].color = 'red'
-            }
-
-            else {
-                players[2].name = "Greg"
-                players[2].turnOrder = 3
-                players[2].color = 'green'
-            }
-            socket.emit('updatePlayers', players, socket.id, currentPlayer)
-            socket.broadcast.emit('notifyPlayers', data)
-    })
-
+io.on('connection', socket => {
     socket.on('disconnect', function(){
-        // const idx = players.findIndex(player => player.id === socket.id)
-        // if (idx > -1){
-        //     players.splice(idx, 1)
-        // }
-        // io.emit('updatePlayers', players, socket.id)
+        //delete rooms[room[socket.id]]
+        //io.emit('updatePlayers', rooms[room], socket.id)
     })
     
-    socket.on('reqTurn', (grid, player) => {
-        players[turnOrder - 1] = player
-        if (turnOrder === players.length){turnOrder = 1}
-        else turnOrder ++
-        currentPlayer = players[turnOrder - 1]
-        io.emit('newGrid', grid)
-        socket.emit('updatePlayers', players, socket.id, currentPlayer)
-        io.emit('newTurn', turnOrder, socket.id, currentPlayer)
+    socket.on('reqTurn', (grid, player, room) => {
+        let currentTurn
+        {room.turnOrder === room.players.length ? currentTurn = 1 : currentTurn = room.turnOrder + 1}
+        io.to(room.id).emit('newGrid', grid)
+        io.to(room.id).emit('newTurn', currentTurn, room)
     })
-
-    socket.on('create', room => {
-        console.log("CREATED", room)
+    socket.on('createRoom', (room)  => {
+        const roomId = room
+        rooms[roomId] = {...rooms[roomId], id: roomId, turnOrder: 1, gameStart: false, players: []}
         socket.join(room)
     })
-
-    socket.on('join', room => {``
+    socket.on('join', room => {
         socket.join(room)
-        io.to(room).emit('opponent_joined')
     })
-
+    socket.on("updateRoom", (room, name, color, isMyTurn, pieces) => {
+        const id = socket.id
+        const player = {id, name, color, isMyTurn, pieces}
+        const currentRoom = rooms[room]
+        currentRoom.players = [...currentRoom.players, player]
+        io.to(room).emit('updatePlayers', rooms[room])
+    })
+    socket.on("startGame", (room) => {
+        rooms[room].gameStart = true
+        io.to(room).emit('gameStart', rooms[room])
+    })
     socket.on('reqRestart', (data) => {
         const room = JSON.parse(data).room
         io.to(room).emit('restart')
